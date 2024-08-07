4833d04c6db86dc71fc97e60be51b063bc2fefd2
Randy <roscal-202379@kinal.edu.gt>
2024-08-07 07:56:30 -0600

    build($version):primera version

---
+++ src/main/java/com/javierherrera/system/SpringBootSistemaRhApplication.java
@@ -0,0 +1,13 @@
+package com.javierherrera.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSistemaRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSistemaRhApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/javierherrera/system/bean/empleado.java
@@ -0,0 +1,14 @@
+package com.javierherrera.system.bean;
+
+public class empleado {
+
+    public empleado() {
+    }
+
+    int id;
+    String nombreCompleto;
+    int dpi;
+    String puesto;
+    Double salario;
+
+}
