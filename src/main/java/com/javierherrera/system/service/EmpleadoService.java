dbf269364f4e7ef5f9fc8071d389ae4422db239f
Randy <roscal-202379@kinal.edu.gt>
2024-08-07 10:22:55 -0600

    feat($EmpleadoService):implementación de métodos abstractos

--- src/main/java/com/javierherrera/system/service/EmpleadoService.java
+++ src/main/java/com/javierherrera/system/service/EmpleadoService.java
@@ -1,5 +1,29 @@
 package com.javierherrera.system.service;
 
-public class EmpleadoService {
+import java.util.List;
 
+import com.javierherrera.system.bean.Empleado;
+
+public class EmpleadoService implements IEmpleadoService {
+
+    @Override
+    public List<Empleado> listarEmpleados() {
+        throw new UnsupportedOperationException("Unimplemented method 'listarEmpleados'");
+    }
+
+    @Override
+    public Empleado buscEmpleado(Integer id) {
+        throw new UnsupportedOperationException("Unimplemented method 'buscEmpleado'");
+    }
+
+    @Override
+    public Empleado guardarEmpleado(Empleado empleado) {
+        throw new UnsupportedOperationException("Unimplemented method 'guardarEmpleado'");
+    }
+
+    @Override
+    public void eliminarEmpleado(Empleado empleado) {
+        throw new UnsupportedOperationException("Unimplemented method 'eliminarEmpleado'");
+    }
+    
 }
